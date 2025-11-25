29dcbe8cacae97a4fdd793438d88762e88d7185f
unknown <leandrolima9214@digitalcollege.internal>
2025-11-25 20:16:04 -0300

    first commit

---
+++ tipos-dados.js
@@ -0,0 +1,80 @@
+//tipo texto - string
+// "Isso é um texto";
+// "Acioly";
+// " O tenis nike é composto...";
+// "132";
+// 'Isso também é um "texto" mesmo com aspas simples'
+
+// `Usar a crase o JS entende como string`
+
+
+//tipo number 
+100 // inteiros - int
+25.35 // decimais - float
+
+
+// tipo booleano - boolean - true / false
+true
+false
+
+// tipo indefinido - undefined
+undefined
+
+// tipo nulo - null
+null
+
+var meuSobrenome = "Silva";  // não usamos mais
+let caraDaCamisaVinho = "Rodrigo"; //usamos
+const altura = 1.9;
+
+// padrão de escrita chamado CamelCase
+
+console.log(caraDaCamisaVinho);
+
+caraDaCamisaVinho = "Anderson";
+
+
+console.log(caraDaCamisaVinho);
+
+
+
+let temCarro = true;
+
+let isOpen = false;
+
+let profissao = undefined;
+
+console.log(profissao);
+//console.log(meuNulo);
+
+
+// crie 3 variáveis da sua escolha com "let" e 3 variáveis com "const".
+
+// depois tente alterar as variaveis com let... e apresente todos os valores no console
+
+
+
+
+let dirigeCarro = true;
+
+let cantaMusica = "sim"
+
+let falaAlto = "nao"
+
+
+const muito = "alto"
+
+const excelente = "afinado"
+
+const demais = "incomoda"
+
+
+dirigeCarro = false;
+
+cantaMusica = "nao";
+
+falaAlto = "sim";
+
+console.log(dirigeCarro);
+console.log(cantaMusica);
+console.log(falaAlto);
